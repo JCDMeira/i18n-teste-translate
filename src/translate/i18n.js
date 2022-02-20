b3b06416b3b67661d87fa4b442ec3ffdb3713674
Jean Meira <jean.meira10@hotmail.com>
2022-02-20 10:59:47 -0300

    fix: detect language error

--- src/translate/i18n.js
+++ src/translate/i18n.js
@@ -1,14 +1,23 @@
 import i18n from "i18next";
+// import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
 import { messages } from "./index";
 
-i18n.use(LanguageDetector).init({
-  debug: false,
-  defaultNS: ["translations"],
-  fallbackLng: "pt",
-  ns: ["translations"],
-  resources: messages,
-});
+// const DETECTION_OPTIONS = {
+//   order: ["navigator"],
+// };
+
+i18n
+  .use(LanguageDetector)
+  // .use(initReactI18next)
+  .init({
+    // detection: DETECTION_OPTIONS,
+    debug: false,
+    defaultNS: ["translations"],
+    fallbackLng: "pt",
+    ns: ["translations"],
+    resources: messages,
+  });
 
 export { i18n };
